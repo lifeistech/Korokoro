fdd02674410eea3896d012718d72699612d04841
asache3 <asache3@gmail.com>
2019-04-19 18:53:30 +0900

    first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + new Vector3(0, 1.0f, -4.0f);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey ("up"))
+        {
+            transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+            Debug.Log("Go");
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position += new Vector3(0, 0, -speed * Time.deltaTime);
+            Debug.Log("Back");
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position += new Vector3(-speed*Time.deltaTime,0,0);
+            Debug.Log("left");
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            Debug.Log("right");
+        }
+    }
+}
