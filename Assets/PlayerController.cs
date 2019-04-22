c73758bb3f417a7eed17442467b58257c9a4040a
nakatomo3 <nakatomo7.0501@gmail.com>
2019-04-23 00:36:27 +0900

    ジャンプ機能を実装

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,38 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    float speed = 3.0f;
+public class PlayerController : MonoBehaviour {
+	float speed = 3.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Rigidbody rigidbody;
+	private float jumpSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey ("up"))
-        {
-            transform.position += new Vector3(0, 0, speed * Time.deltaTime);
-            Debug.Log("Go");
-        }
-        if (Input.GetKey("down"))
-        {
-            transform.position += new Vector3(0, 0, -speed * Time.deltaTime);
-            Debug.Log("Back");
-        }
-        if (Input.GetKey("left"))
-        {
-            transform.position += new Vector3(-speed*Time.deltaTime,0,0);
-            Debug.Log("left");
-        }
-        if (Input.GetKey("right"))
-        {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
-            Debug.Log("right");
-        }
-    }
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey("up")) {
+			transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+			Debug.Log("Go");
+		}
+		if (Input.GetKey("down")) {
+			transform.position += new Vector3(0, 0, -speed * Time.deltaTime);
+			Debug.Log("Back");
+		}
+		if (Input.GetKey("left")) {
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			Debug.Log("left");
+		}
+		if (Input.GetKey("right")) {
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			Debug.Log("right");
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rigidbody.velocity = new Vector3(0, jumpSpeed, 0);
+		}
+	}
 }
