84161638aacc06a0928272e5f81e4d4618cbec9b
Yua Akimoto <>
2019-04-23 21:20:42 +0900

    add script

---
+++ Assets/Yuai/floorGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorGenerator : MonoBehaviour {
+
+	bool floor_gene = true;
+	float floor_posX = 0;
+	float floor_posZ = 0;
+	Vector3 new_floorPos;
+	public GameObject floor;
+	// Use this for initialization
+	void Start () {
+		floor_posX = transform.position.x + Random.Range (-0.7f, 0.7f);
+		floor_posZ = transform.position.z + 1;
+		new_floorPos = new Vector3(floor_posX, 0, floor_posZ);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnCollisionEnter (Collision other) {
+		if (other.gameObject.CompareTag ("Player") && floor_gene) {
+			Instantiate (floor, new_floorPos, transform.rotation);
+			floor_gene = false;
+		}
+		else
+		{
+			return;
+		}
+	}
+	public void OnCollisionExit (Collision other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy (this.gameObject);
+		}else
+		{
+			return;
+		}
+	}
+}
